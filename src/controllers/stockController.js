5bd93d1e5912ca90d4163e1af5c594f9ed9fa4ee
Carolina Lunas <carolinalunasfarah@gmail.com>
2025-03-24 16:07:04 -0300

    rearrange importations

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -1,7 +1,7 @@
 // model
 import { saveStock, getAllSymbols, getBySymbol } from "../models/stockModel.js";
 
-// framework
+// package
 import yahooFinance from "yahoo-finance2";
 
 export const getStockData = async (req, res) => {
